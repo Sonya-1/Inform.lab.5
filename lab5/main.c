3dee936871ee3e26164dde3c859545a3ea3c0272
Sonya-1 <sofi.smorodskaya@gmail.com>
2020-11-04 14:01:35 +0300

    fixed

--- lab5/main.c
+++ lab5/main.c
@@ -1,9 +1,26 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
-void add_gaps(char* str) {
+//Ввести строку. Вывести строку так, чтобы за каждым словом следовало количество пробелов, равное длине слова.
+
+int char_index(char c, char* str) {
+	int i;
+
+	for (i = 0; str[i] > 0; i++) {
+		if (c == str[i]) {
+			return i;
+		}
+
+	}
+	if (c == 0) {
+		return i;
+	}
+	return -1;
+}
+
+void add_gaps(char* str, char* separators) {
 	int i = 0, j, count = 0, k = 0;
 	while (str[i] != 0) {
-		if (str[i] == ' ' || str[i] == '\n' || str[i] == '\0' || str[i] == '\t') {
+		if (char_index(str[i], separators) >= 0) {
 			for (j = count; j != 0; j--) {
 				printf("%c", ' ');
 			}
@@ -29,9 +46,11 @@ void add_gaps(char* str) {
 int main()
 {
 	char str[256];
+	char* separators = " \t\n,.;:!?/~`@#$%^&*(){}[]\"|'/<>-=_+";
+
 	printf("Input string: ");
 	fgets(str, 256, stdin);
-	add_gaps(str);
+	add_gaps(str, separators);
 
 	return 0;
 }
